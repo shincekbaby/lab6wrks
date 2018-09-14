7e7e2c43c33c1bf963c7b77755fc2dbdbe016ae0
shincekbaby <42138520+shincekbaby@users.noreply.github.com>
2018-09-14 06:39:56 +0000

    Delete lab6_q1.cpp

--- lab6_q1.cpp
+++
@@ -1,7 +0,0 @@
-//include the library files and start main
-#include <iostream>
-using namespace std;
-
-int main()
-{//declare varibles required	
-	float unitCost,unitsPurch,taxRate,salesTax,totalDue;
